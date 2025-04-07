def52d72f481dd7a7ead538d70971413e0fdb4c7
Gwapobenjie <plazabenjie1@gmail.com>
2025-04-07 14:31:04 +0800

    fixed gh-pages

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/backtostart/", 
+  base: "/backtostart/",  // Ensure this is the correct base URL for your GitHub Pages
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',  // Make sure the output is directed to 'dist' folder
   },
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:9000", 
+        target: "http://127.0.0.1:9000",  // Your backend server URL
         changeOrigin: true,
         secure: false,
       },
